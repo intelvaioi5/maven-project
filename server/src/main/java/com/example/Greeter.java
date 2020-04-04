e8aaf8f3da8cb120532133e203e7c33c38c485e6
unknown <tbalkawade@gmail.com>
2020-04-04 17:40:08 +0530

    commit for checksum

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ /**
+ Someone the name of person
+ */
+ 
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
